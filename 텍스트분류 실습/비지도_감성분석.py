ba89468710c56c636de5a193aaa007e31097275c
hyeok jin son <hjson817@gmail.com>
2020-10-13 21:35:10 +0900

    NLP실습

--- 텍스트분류 실습/비지도_감성분석.py
+++ 텍스트분류 실습/비지도_감성분석.py
@@ -37,3 +37,100 @@ for entity in entities:
 # 개별 단어별 synset과 다른 단어의 synset과의 유사도를 DataFrame형태로 저장합니다.
 similarity_df = pd.DataFrame(similarities, columns=entity_names, index=entity_names)
 similarity_df
+
+import nltk
+from nltk.corpus import sentiwordnet as swn
+
+senti_synsets = list(swn.senti_synsets('slow'))
+print('senti_synsets() 반환 type :', type(senti_synsets))
+print('senti_synsets() 반환 값 갯수:', len(senti_synsets))
+print('senti_synsets() 반환 값 :', senti_synsets)
+
+
+import nltk
+from nltk.corpus import sentiwordnet as swn
+
+father = swn.senti_synset('father.n.01')
+print('father 긍정감성 지수: ', father.pos_score())
+print('father 부정감성 지수: ', father.neg_score())
+print('father 객관성 지수: ', father.obj_score())
+print('\n')
+fabulous = swn.senti_synset('fabulous.a.01')
+print('fabulous 긍정감성 지수: ',fabulous .pos_score())
+print('fabulous 부정감성 지수: ',fabulous .neg_score())
+
+from nltk.corpus import wordnet as wn
+
+# 간단한 NTLK PennTreebank Tag를 기반으로 WordNet기반의 품사 Tag로 변환
+def penn_to_wn(tag):
+    if tag.startswith('J'):
+        return wn.ADJ
+    elif tag.startswith('N'):
+        return wn.NOUN
+    elif tag.startswith('R'):
+        return wn.ADV
+    elif tag.startswith('V'):
+        return wn.VERB
+    return
+
+
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import sentiwordnet as swn
+from nltk import sent_tokenize, word_tokenize, pos_tag
+
+
+def swn_polarity(text):
+    # 감성 지수 초기화
+    sentiment = 0.0
+    tokens_count = 0
+
+    lemmatizer = WordNetLemmatizer()
+    raw_sentences = sent_tokenize(text)
+    # 분해된 문장별로 단어 토큰 -> 품사 태깅 후에 SentiSynset 생성 -> 감성 지수 합산
+    for raw_sentence in raw_sentences:
+        # NTLK 기반의 품사 태깅 문장 추출
+        tagged_sentence = pos_tag(word_tokenize(raw_sentence))
+        for word, tag in tagged_sentence:
+
+            # WordNet 기반 품사 태깅과 어근 추출
+            wn_tag = penn_to_wn(tag)
+            if wn_tag not in (wn.NOUN, wn.ADJ, wn.ADV):
+                continue
+            lemma = lemmatizer.lemmatize(word, pos=wn_tag)
+            if not lemma:
+                continue
+            # 어근을 추출한 단어와 WordNet 기반 품사 태깅을 입력해 Synset 객체를 생성.
+            synsets = wn.synsets(lemma, pos=wn_tag)
+            if not synsets:
+                continue
+            # sentiwordnet의 감성 단어 분석으로 감성 synset 추출
+            # 모든 단어에 대해 긍정 감성 지수는 +로 부정 감성 지수는 -로 합산해 감성 지수 계산.
+            synset = synsets[0]
+            swn_synset = swn.senti_synset(synset.name())
+            sentiment += (swn_synset.pos_score() - swn_synset.neg_score())
+            tokens_count += 1
+
+    if not tokens_count:
+        return 0
+
+    # 총 score가 0 이상일 경우 긍정(Positive) 1, 그렇지 않을 경우 부정(Negative) 0 반환
+    if sentiment >= 0:
+        return 1
+
+    return 0
+import pandas as pd
+
+review_df = pd.read_csv('./labeledTrainData.tsv', header=0, sep="\t", quoting=3)
+review_df.head(3)
+
+review_df['preds'] = review_df['review'].apply( lambda x : swn_polarity(x) )
+y_target = review_df['sentiment'].values
+preds = review_df['preds'].values
+
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score
+from sklearn.metrics import recall_score, f1_score, roc_auc_score
+
+print(confusion_matrix( y_target, preds))
+print("정확도:", accuracy_score(y_target , preds))
+print("정밀도:", precision_score(y_target , preds))
+print("재현율:", recall_score(y_target, preds))
